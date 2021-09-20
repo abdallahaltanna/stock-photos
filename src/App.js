7b8ebd9fc51a6e2e55f6968b5b26e61d2232174d
abdallahaltanna <abdallahmyaltanna@gmail.com>
2021-09-20 20:28:22 +0300

    add files

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Photo from './Photo';
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPage(1);
+  };
+
+  const fetchPhotos = async () => {
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+    let url;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
+
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        }
+        if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+      });
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+    // eslint-disable-next-line
+  }, [page]);
+
+  useEffect(() => {
+    const event = window.addEventListener('scroll', () => {
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+    return window.removeEventListener('scroll', event);
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <main>
+      <section className='search'>
+        <form className='search-form'>
+          <input
+            type='text'
+            className='form-input'
+            placeholder='search'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button className='submit-btn' onClick={handleSubmit} type='submit'>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((image, index) => {
+            return <Photo key={index} {...image} />;
+          })}
+        </div>
+        {loading && <h2 className='loading'>loading...</h2>}
+      </section>
+    </main>
+  );
+}
+
+export default App;
